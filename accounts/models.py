34afd4c37892769748cbb7f1066247a0410dda73
berrybab6 <bedatuassefa@gmail.com>
2021-03-12 21:59:53 +0300

    account feature added

---
+++ accounts/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager)
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, is_active=True, is_staff=False, is_admin=False):
+        if not email:
+            raise ValueError("User Must have an email address")
+        if not password:
+            raise  ValueError("User Must have a Password")
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.active = is_active
+        user.admin = is_admin
+        user.staff = is_staff
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(email, password=password, is_staff=True, is_admin=True)
+        return user
+    def create_staffuser(self, email, password=None):
+        user = self.create_user(email, password=password, is_staff=True)
+        return user
+# Create User models here.
+class User(AbstractBaseUser):
+    email = models.CharField(unique=True, max_length=255, default=False)
+    # full_name = models.CharField(max_length=255, blank=True, null=True)
+    active = models.BooleanField(default=True)
+    staff = models.BooleanField(default=False)
+    admin = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+    def __str__(self):
+        return self.email
+    def get_full_name(self):
+        return self.email
+    def get_short_name(self):
+        return self.email
+    def has_perm(self, perm, obj=None):
+        return True
+    def has_module_perms(self, app_label):
+        return True
+    @property
+    def is_staff(self):
+        return self.staff
+    @property
+    def is_admin(self):
+        return self.admin
+    
