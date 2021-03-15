d1d735a139a04c7941182dc3ef4abadc91737a77
berrybab6 <bedatuassefa@gmail.com>
2021-03-15 19:10:13 +0300

    tikus api urls updated

--- tikusapi/urls.py
+++ tikusapi/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    # path('users/', include('accounts.urls')),
+    path('admin/', admin.site.urls),
+    path('users/', include('accounts.urls')),
     path('', include('frontend.urls')),
     path('', include('event.urls'))
 ]
